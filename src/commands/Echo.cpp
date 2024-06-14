36c888338f34aca9ffcca624e16dd22cb53299f1
Dulinniel <nightingalenervous@gmail.com>
2024-06-14 20:50:12 +0200

    Remove useless default parameters

--- src/commands/Echo.cpp
+++ src/commands/Echo.cpp
@@ -6,7 +6,7 @@
 class Echo : public Command 
 {
 public:
-  void execute(const std::deque<std::string>& args, [[maybe_unused]] const std::unordered_map<std::string, std::string>& known_commands = {}) override 
+  void execute(const std::deque<std::string>& args, [[maybe_unused]] const std::unordered_map<std::string, std::string>& known_commands) override 
   {
     std::string text = implode_deque(args);
     std::cout << text << std::endl;
--- src/commands/Exit.cpp
+++ src/commands/Exit.cpp
@@ -5,7 +5,7 @@
 class Exit : public Command 
 {
 public:
-  void execute(const std::deque<std::string>& args, [[maybe_unused]] const std::unordered_map<std::string, std::string>& known_commands = {}) override 
+  void execute(const std::deque<std::string>& args, [[maybe_unused]] const std::unordered_map<std::string, std::string>& known_commands) override 
   {
     int status = std::stoi(args[1]);
     std::exit(status);
